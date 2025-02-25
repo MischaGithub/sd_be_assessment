ffc998f92427c608a5811db78a9c6ba6664b4520
MischaGithub <mischa.achmat120212@gmail.com>
2025-02-25 22:59:08 +0200

    started unit test functionality

--- test_league_ranking.py
+++ test_league_ranking.py
@@ -1,6 +1,25 @@
 import unittest
 from league_ranking import process_results, get_match_details
 
+class TestLeagueRanking(unittest.TestCase):
+    def test_basic_ranking(self):
+        matches = [
+            "Liverpool 3, Madrid 3",
+            "Aston Villa 1, Man Utd 0",
+            "Liverpool 1, Man Utd 1",
+            "Aston Villa 3, Madrid 1",
+            "Liverpool 4, Man City 0"
+        ]
+        expected_output = (
+            "1. Liverpool, 7 pts\n"
+            "2. Aston Villa, 6 pts\n"
+            "3. Madrid, 4 pts\n"
+            "4. Man Utd, 1 pt\n"
+            "5. Man City, 0 pts"
+        )
+        self.assertEqual(process_results(matches), expected_output)
+    
+
 def test_invalid_input(self):
         with self.assertRaises(ValueError):
             get_match_details("Invalid Data Here")
